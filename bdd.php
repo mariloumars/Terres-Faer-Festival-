3571fa62c01c245c538c4180738954c262d10b22
TaupeInHambourg <129371488+TaupeInHambourg@users.noreply.github.com>
2023-03-30 09:58:38 +0200

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -20,11 +20,11 @@
         $sQuery = 'INSERT INTO reservation (Nom,Prenom,Mail,Telephone,nb_adulte,nb_enfant)
         VALUES ("'.$nom.'", "'.$prenom.'", "'.$mail.'", "'.$tel.'", "'.$adulte.'", "'.$enfant.'")';
 
-        if($tel == 0){
+        if($tel == "Entrer votre numéro de téléphone"){
             $sQuery='INSERT INTO reservation (Nom,Prenom,Mail,nb_adulte,nb_enfant) VALUES ("'.$nom.'", "'.$prenom.'", "'.$mail.'", "'.$adulte.'", "'.$enfant.'")';}
-        if($prenom == "test"){
+        if($prenom == "Entrer votre prénom"){
             $sQuery='INSERT INTO reservation (Nom,Mail,Telephone,nb_adulte,nb_enfant) VALUES ("'.$nom.'", "'.$mail.'", "'.$tel.'", "'.$adulte.'", "'.$enfant.'")';}
-        if($prenom == "test" AND $tel == 0){
+        if($prenom == "Entrer votre prénom" AND $tel == "Entrer votre numéro de téléphone"){
             $sQuery='INSERT INTO reservation (Nom,Mail,nb_adulte,nb_enfant) VALUES ("'.$nom.'", "'.$mail.'", "'.$adulte.'", "'.$enfant.'")';}
         
         echo `<p class="bold">Votre réservation à bien été enregistrer</p>` ;
